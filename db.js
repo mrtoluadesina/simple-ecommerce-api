c77a11613955e612a6f734860ef2bcc14a197569
Tolu Adesina <tadesina90@gmail.com>
2019-04-12 17:15:10 +0100

    Created my db array with 3 user objects

---
+++ db.js
@@ -0,0 +1,7 @@
+var users = [
+  {name: "tomi", email: "aadesina92@gmail.com", password: "qwerty1", id: 1},
+  {name: "tolu", email: "tadesina90@gmail.com", password: "qwerty", id: 2},
+  {name: "izu", email: "izukuzu@gmail.com", password: "izzyu", id: 3}
+];
+
+module.exports = users;

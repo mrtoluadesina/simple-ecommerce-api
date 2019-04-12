7e8dbf5d3050f7485da9d0b6a5332d6d68c990f5
Tolu Adesina <tadesina90@gmail.com>
2019-04-12 17:17:15 +0100

    .gitignore file update to ignore the coverage folder

--- users/user.js
+++ users/user.js
@@ -1,1 +1,42 @@
-var users = [];
+var users = require('../db');
+
+function User() {
+  
+}
+User.prototype.createUser = function(name, email, password) {
+  console.log('User created');
+  users.push({
+    name : this.name,
+    email: this.email,
+    password: this.email,
+    id: users.length > 0 ? users[users.length - 1].id + 1 : 1
+  });
+  return 'User Created';
+}
+User.prototype.readUserById = function(id) {
+  console.log(id);
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].id === id) {
+      console.log(users[i]);
+      return users[i];
+    }
+  }
+}
+User.prototype.update = function(id, name, email, password) {
+  console.log('update done!');
+  var person = User.readUserById(id);
+  person.name = name;
+  person.email = email;
+  person.password = password;
+}
+User.prototype.search = function(name) {
+  console.log(name);
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].name === name) {
+      console.log(users[i]);
+      return users[i];
+    }
+  }
+}
+
+module.exports = User;

f488ff506b15ab5fc870144e77dc1e3e46525608
Tolu Adesina <tadesina90@gmail.com>
2019-04-13 18:52:01 +0100

    Admin object updated with the correct db path for functionalities

--- users/admin.js
+++ users/admin.js
@@ -10,23 +10,23 @@ Admin.prototype.constructor = Admin;
 
 Admin.prototype.readAllUsers = function() {
   console.log('All the Users');
-  console.log(db);
-  return db;
+  console.log(db.users);
+  return db.users;
 }
 
 Admin.prototype.deleteAUser = function(id) {
   console.log('Delete A user');
   var toDelete = Admin.prototype.readUserById(id)
-  var who = db.indexOf(toDelete);
-  db.splice(who,1);
-  console.log(db);
+  var who = db.users.indexOf(toDelete);
+  db.users.splice(who,1);
+  console.log(db.users);
   return 'User Deleted';
 }
 
 Admin.prototype.deleteAllUsers = function() {
   console.log('Delete All Users');
-  db.length = 0;
-  console.log(db);
+  db.users.length = 0;
+  console.log(db.users);
   return 'All Users Deleted';
 }
 

00ff258d203ad6f14d3a4ddb29c3f360c0dac90e
Tolu Adesina <tadesina90@gmail.com>
2019-04-13 05:55:33 +0100

    User createUser method dumbed down to es5 version of passing values to keys in objects, when the name is the same

--- users/user.js
+++ users/user.js
@@ -6,9 +6,9 @@ function User() {
 User.prototype.createUser = function(name, email, password) {
   console.log('User created');
   db.push({
-    name,
-    email,
-    password,
+    name: name,
+    email: email,
+    password: password,
     id: db.length > 0 ? db[db.length - 1].id + 1 : 1
   });
   return 'User Created';

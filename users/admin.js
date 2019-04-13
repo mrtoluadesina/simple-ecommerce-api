e3c1eb4d4562f26f8d246ef0fbf531ea8b711a29
Tolu Adesina <tadesina90@gmail.com>
2019-04-13 04:56:51 +0100

    Delete a user method created and added to the Admin Object - tested in browser

--- users/admin.js
+++ users/admin.js
@@ -14,8 +14,13 @@ Admin.prototype.readAllUsers = function() {
   return db;
 }
 
-Admin.prototype.deleteAUser = function() {
+Admin.prototype.deleteAUser = function(id) {
   console.log('Delete A user');
+  var toDelete = Admin.prototype.readUserById(id)
+  var who = db.indexOf(toDelete);
+  db.splice(who,1);
+  console.log(db);
+  return db;
 }
 
 Admin.prototype.deleteAllUsers = function() {

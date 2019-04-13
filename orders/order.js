c4303ba31c14b39b649c75474bfa891aee3fa1c3
Tolu Adesina <tadesina90@gmail.com>
2019-04-13 19:06:18 +0100

    Function created to solve edge case checking instance of the construct

--- orders/order.js
+++ orders/order.js
@@ -1,7 +1,14 @@
 var db = require('../db');
 
 function Order() {
-  
+  var date = new Date();
+
+  this.date = date.toDateString();
+  this.time = date.toTimeString();
+  this.id = db.orders.length > 0 ? db.orders[db.orders.length - 1].id + 1 : 1
+}
+Order.prototype.createOrder = function() {
+  return new Order;
 }
 
 module.exports = Order;

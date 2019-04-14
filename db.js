b729c02ea8ed94e01d33cf349a17f56da0d72335
Tolu Adesina <tadesina90@gmail.com>
2019-04-14 05:55:11 +0100

    Added records to the orders db, to aid testing with dummy values (fakes)

--- db.js
+++ db.js
@@ -4,7 +4,20 @@ var users = [
   {name: "izu", email: "izukuzu@gmail.com", password: "izzyu", id: 3},
   {name: "ive", email: "iveotv@gmail.com", password: "ivesmile", id: 4}
 ];
-var orders = []
+var orders = [
+  {
+    date: 'Sat Apr 13 2019',
+    time: '10:20:13 GMT+0100 (West Africa Standard Time)',
+    id: 1,
+    user_id: 2,
+    products: [ 'jeans', 'polo' ] },
+  {
+    date: 'Sun Apr 14 2019',
+    time: '05:44:23 GMT+0100 (West Africa Standard Time)',
+    id: 2,
+    user_id: 1,
+    products: [ 'bag' ] }
+]
 
 module.exports = {
   users, orders

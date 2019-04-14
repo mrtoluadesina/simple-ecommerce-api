a4c642716f9ca66dd2490b43b3e12a45ad3b52ff
Tolu Adesina <tadesina90@gmail.com>
2019-04-14 08:51:18 +0100

    Test Case - Test cases for reading and deleting all orders added to the orders test case

--- orders/order.test.js
+++ orders/order.test.js
@@ -5,3 +5,14 @@ test('Finding instance of the construct', function() {
   var od = new order();
   expect(od instanceof order).toBeTruthy();
 });
+
+test('Read all orders', function() {
+  var od = new order();
+  expect(od.readAllOrders()).toBe(db.orders);
+});
+
+test('Delete All Orders', function() {
+  var od = new order();
+  expect(od.deleteAllOrders()).toBeTruthy();
+  expect(db.orders.length).toBe(0);
+});

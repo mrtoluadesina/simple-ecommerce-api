08f06b1a39f58404c3ef5be49fac28305f0447da
Tolu Adesina <tadesina90@gmail.com>
2019-04-15 08:10:40 +0100

    Order function to perform order details update

--- orders/order.js
+++ orders/order.js
@@ -25,11 +25,19 @@ Order.prototype.readOrderById = function(id) {
 }
 Order.prototype.editOrderDetails = function() {
   console.log('updating ... ');
-
+  var order = Order.prototype.readOrderById(this.id);
+  console.log(order);
+  order.products = arguments[0];
+  console.log(order.products);
+  return 'Order Edited';
 }
-Order.prototype.deleteAnOrder = function() {
+Order.prototype.deleteAnOrder = function(id) {
   console.log('Delete one order!');
-  
+  var toDelete = Order.prototype.readOrderById(id);
+  var what = db.orders.indexOf(toDelete);
+  db.orders.splice(what, 1);
+  console.log(db.orders);
+  return 'Order Deleted';
 }
 Order.prototype.deleteAllOrders = function() {
   console.log('Delete All Orders !');
